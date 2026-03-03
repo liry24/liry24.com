8f1c7c4e6c53a7fde7893bd342def353bcf679a9
Liry24 <liry241999@gmail.com>
2026-03-03 15:53:33 +0900

    refactor: enhance SEO utility to support dynamic Open Graph images and improve type safety

--- apps/home/app/components/OgImage/Home.takumi.vue
+++ apps/home/app/components/OgImage/Home.takumi.vue
@@ -9,7 +9,7 @@ const { title = 'title' } = defineProps<Props>()
     <div
         class="flex size-full flex-col items-center justify-center gap-16 bg-black font-[Special_Gothic_Expanded_One]"
     >
-        <h1 class="pt-8 text-left text-9xl font-black text-white">
+        <h1 class="pt-8 text-9xl tracking-tight text-white">
             {{ title }}
         </h1>
         <p class="text-2xl text-zinc-400">liry24.com</p>
